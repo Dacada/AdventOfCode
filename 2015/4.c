645405303e42683ce49aa338c0aee422adc4b100
dacada <dacada@whatever>
2019-01-20 01:04:34 +0100

    Finished day 4 of 2015

---
+++ 2015/4.c
@@ -0,0 +1,38 @@
+#include <aoclib.h>
+#include <bsd/md5.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+static void solution(char *input, char *output, uint8_t digest2_val) {
+  size_t len = strlen(input);
+  MD5_CTX base_ctx, iter_ctx;
+  MD5Init(&base_ctx);
+  MD5Update(&base_ctx, (uint8_t*)input, len);
+
+  for (unsigned long result = 0; result < ULONG_MAX; result++) {
+    memcpy(&iter_ctx, &base_ctx, sizeof(MD5_CTX));
+    
+    sprintf(output, "%lu", result);
+    len = strlen(output);
+    MD5Update(&iter_ctx, (uint8_t*)output, len);
+    
+    uint8_t digest[MD5_DIGEST_LENGTH];
+    MD5Final(digest, &iter_ctx);
+    if (digest[0] == 0 && digest[1] == 0 && digest[2] <= digest2_val)
+      break;
+  }
+}
+
+static void solution1(char *input, char *output) {
+  solution(input, output, 0xf);
+}
+
+static void solution2(char *input, char *output) {
+  solution(input, output, 0);
+}
+
+int main(int argc, char *argv[]) {
+  return aoc_run(argc, argv, solution1, solution2);
+}
--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Wformat -Wshadow -Wpointer-arith -Wcast-qual -Wmissing-prototypes -std=c99 -Ofast
 LDFLAGS = -Wall -Wextra -std=c99 -Ofast
+LDLIBS =
 
 aoclib/aoclib.o: aoclib/aoclib.c
 	$(CC) $(CFLAGS) -c $^ -o $@
@@ -8,5 +9,6 @@ aoclib/aoclib.o: aoclib/aoclib.c
 2015/%.o: 2015/%.c
 	$(CC) $(CFLAGS) -Iaoclib -c $^ -o $@
 
+2015/4: LDLIBS = -lbsd
 2015/%: 2015/%.o aoclib/aoclib.o
-	$(CC) $(LDFLAGS) $^ -o $@
+	$(CC) $(LDFLAGS) $(LDLIBS) $^ -o $@
