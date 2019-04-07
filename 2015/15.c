d831b0d43540135f33e11ef29ea4d347f2c9fda6
dacada <dacada@whatever>
2019-04-07 20:08:46 +0200

    Finished days 15 and 16

---
+++ 2015/15.c
@@ -0,0 +1,161 @@
+#include <aoclib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define NINGREDIENTS 4
+
+#define ISNUM(n) ((n) >= '0' && (n) <= '9')
+#define ZERO_IF_NEG(n) ((n) < 0 ? 0 : (n))
+
+struct ingredient_t {
+  int capacity;
+  int durability;
+  int flavor;
+  int texture;
+  int calories;
+};
+
+struct ingredient_t ingredients[NINGREDIENTS];
+
+static int parse_next_number(char *input, int *i) {
+  int j = *i;
+  while (!ISNUM(input[j]) && input[j] != '-') {
+    j++;
+  }
+
+  bool negative = input[j] == '-';
+  if (negative) {
+    j++;
+  }
+
+  int num = 0;
+  while (ISNUM(input[j])) {
+    num = num*10 + input[j]-0x30;
+    j++;
+  }
+
+  *i = j;
+
+  if (negative) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+static void parse_line(char *input, int *i, int j) {
+  ingredients[j].capacity = parse_next_number(input, i);
+  ingredients[j].durability = parse_next_number(input, i);
+  ingredients[j].flavor = parse_next_number(input, i);
+  ingredients[j].texture = parse_next_number(input, i);
+  ingredients[j].calories = parse_next_number(input, i);
+}
+
+static void parse(char *input) {
+  for (int i=0, j=0;; i++) {
+    if (input[i] == '\0') {
+      break;
+    }
+    
+    parse_line(input, &i, j);
+    j++;
+    ASSERT(input[i] == '\n', "Did not parse full line");
+  }
+}
+
+static void iter_shares(unsigned int elements, unsigned int total, unsigned int *share, size_t i, void(*fun)(unsigned int*, void*), void *args) {
+  if (elements > total) {
+    FAIL("Attempt to call iter shares with more elements to share with than things to share");
+  }
+  else if (elements < 2) {
+    FAIL("Attempt to call iter shares with less than two elements");
+  } else if (elements == 2) {
+    for (unsigned int n=1; n<total; n++) {
+      unsigned int m = total - n;
+      share[i] = n;
+      share[i+1] = m;
+      fun(share, args);
+    }
+  } else {
+    for (unsigned int n=1; n<total-elements+2; n++) {
+      unsigned int m = total - n;
+      share[i] = n;
+      iter_shares(elements - 1, m, share, i+1, fun, args);
+    }
+  }
+}
+
+static unsigned int calc_share_value(unsigned int *share) {
+  int total_capacity = 0;
+  int total_durability = 0;
+  int total_flavor = 0;
+  int total_texture = 0;
+  
+  for (int i=0; i<NINGREDIENTS; i++) {
+    total_capacity += ingredients[i].capacity * share[i];
+    total_durability += ingredients[i].durability * share[i];
+    total_flavor += ingredients[i].flavor * share[i];
+    total_texture += ingredients[i].texture * share[i];
+  }
+  
+  return ZERO_IF_NEG(total_capacity) * ZERO_IF_NEG(total_durability) * ZERO_IF_NEG(total_flavor) * ZERO_IF_NEG(total_texture);
+}
+
+static unsigned int calc_share_value_if_500cal(unsigned int *share) {
+  int total_calories = 0;
+  for (int i=0; i<NINGREDIENTS; i++) {
+    total_calories += ingredients[i].calories * share[i];
+  }
+
+  if (total_calories == 500) {
+    return calc_share_value(share);
+  } else {
+    return 0;
+  }
+}
+
+static void find_max_share(unsigned int *share, void *args) {
+  unsigned int *max_ptr = args;
+  unsigned int max = *max_ptr;
+  unsigned int share_val = calc_share_value(share);
+  if (share_val > max)
+    *max_ptr = share_val;
+}
+
+static void find_max_share_500cal(unsigned int *share, void *args) {
+  unsigned int *max_ptr = args;
+  unsigned int max = *max_ptr;
+  unsigned int share_val = calc_share_value_if_500cal(share);
+  if (share_val > max)
+    *max_ptr = share_val;
+}
+
+static unsigned int solve1(void) {
+  unsigned int max = 0;
+  unsigned int share[NINGREDIENTS];
+  iter_shares(NINGREDIENTS, 100, share, 0, find_max_share, &max);
+  return max;
+}
+
+static unsigned int solve2(void) {
+  unsigned int max = 0;
+  unsigned int share[NINGREDIENTS];
+  iter_shares(NINGREDIENTS, 100, share, 0, find_max_share_500cal, &max);
+  return max;
+}
+
+static void solution1(char *input, char *output) {
+  parse(input);
+  unsigned int result = solve1();
+  snprintf(output, OUTPUT_BUFFER_SIZE, "%u", result);
+}
+
+static void solution2(char *input, char *output) {
+  parse(input);
+  unsigned int result = solve2();
+  snprintf(output, OUTPUT_BUFFER_SIZE, "%u", result);
+}
+
+int main(int argc, char *argv[]) {
+  return aoc_run(argc, argv, solution1, solution2);
+}
---
+++ 2015/16.c
@@ -0,0 +1,198 @@
+#include <aoclib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define CHILDREN 3
+#define CATS 7
+#define SAMOYEDS 2
+#define POMERANIANS 3
+#define AKITAS 0
+#define VIZSLAS 0
+#define GOLDFISH 5
+#define TREES 3
+#define CARS 2
+#define PERFUMES 1
+
+#define ISNUM(n) ((n) >= '0' && (n) <= '9')
+
+static bool(*akitas_ok)(unsigned int n);
+static bool(*cars_ok)(unsigned int n);
+static bool(*cats_ok)(unsigned int n);
+static bool(*children_ok)(unsigned int n);
+static bool(*goldfish_ok)(unsigned int n);
+static bool(*perfumes_ok)(unsigned int n);
+static bool(*pomeranians_ok)(unsigned int n);
+static bool(*samoyeds_ok)(unsigned int n);
+static bool(*trees_ok)(unsigned int n);
+static bool(*vizslas_ok)(unsigned int n);
+
+static bool akitas_ok1(unsigned int n)      { return n == AKITAS; }
+static bool cars_ok1(unsigned int n)        { return n == CARS; }
+static bool cats_ok1(unsigned int n)        { return n == CATS; }
+static bool children_ok1(unsigned int n)    { return n == CHILDREN; }
+static bool goldfish_ok1(unsigned int n)    { return n == GOLDFISH; }
+static bool perfumes_ok1(unsigned int n)    { return n == PERFUMES; }
+static bool pomeranians_ok1(unsigned int n) { return n == POMERANIANS; }
+static bool samoyeds_ok1(unsigned int n)    { return n == SAMOYEDS; }
+static bool trees_ok1(unsigned int n)       { return n == TREES; }
+static bool vizslas_ok1(unsigned int n)     { return n == VIZSLAS; }
+
+static bool akitas_ok2(unsigned int n)      { return n == AKITAS; }
+static bool cars_ok2(unsigned int n)        { return n == CARS; }
+static bool cats_ok2(unsigned int n)        { return n > CATS; }
+static bool children_ok2(unsigned int n)    { return n == CHILDREN; }
+static bool goldfish_ok2(unsigned int n)    { return n < GOLDFISH; }
+static bool perfumes_ok2(unsigned int n)    { return n == PERFUMES; }
+static bool pomeranians_ok2(unsigned int n) { return n < POMERANIANS; }
+static bool samoyeds_ok2(unsigned int n)    { return n == SAMOYEDS; }
+static bool trees_ok2(unsigned int n)       { return n > TREES; }
+static bool vizslas_ok2(unsigned int n)     { return n == VIZSLAS; }
+
+static unsigned int parse_number(char *input, unsigned int *i) {
+  unsigned int j = *i;
+  while (!ISNUM(input[j])) {
+    j++;
+  }
+  unsigned int num = 0;
+  while (ISNUM(input[j])) {
+    num = num*10 + input[j]-0x30;
+    j++;
+  }
+  *i = j;
+  return num;
+}
+
+static bool parse_pair(char *input, unsigned int *i) {
+  unsigned int j = *i;
+  while (input[j] < 'a' || input[j] > 'z') {
+    j++;
+  }
+
+  bool ret;
+  if (input[j] == 'a') { // akitas
+    j += 6;
+    ret = akitas_ok(parse_number(input, &j));
+  } else if (input[j] == 'c') { // cars, cats or children
+    j += 2;
+    if (input[j] == 'r') { // cars
+      j += 2;
+      ret = cars_ok(parse_number(input, &j));
+    } else if (input[j] == 't') { // cats
+      j += 2;
+      ret = cats_ok(parse_number(input, &j));
+    } else if (input[j] == 'i') { // children
+      j += 6;
+      ret = children_ok(parse_number(input, &j));
+    } else {
+      FAIL("Unexpected key");
+    }
+  } else if (input[j] == 'g') { // goldfish
+    j += 8;
+    ret = goldfish_ok(parse_number(input, &j));
+  } else if (input[j] == 'p') { // perfumes, pomeranians
+    j += 1;
+    if (input[j] == 'e') { // perfumes
+      j += 7;
+      ret = perfumes_ok(parse_number(input, &j));
+    } else if (input[j] == 'o') { // pomeranians
+      j += 10;
+      ret = pomeranians_ok(parse_number(input, &j));
+    } else {
+      FAIL("Unexpected key");
+    }
+  } else if (input[j] == 's') { // samoyeds
+    j += 8;
+    ret = samoyeds_ok(parse_number(input, &j));
+  } else if (input[j] == 't') { // trees
+    j += 5;
+    ret = trees_ok(parse_number(input, &j));
+  } else if (input[j] == 'v') { // vizslas
+    j += 7;
+    ret = vizslas_ok(parse_number(input, &j));
+  } else {
+    FAIL("Unexpected key");
+  }
+
+  *i = j;
+  return ret;
+}
+
+static void skip_to_eol(char *input, unsigned int *i) {
+  int j = *i;
+  while (input[j] != '\n') {
+    j++;
+  }
+  *i = j;
+}
+
+static unsigned int parse_line(char *input, unsigned int *i) {
+  unsigned int aunt = parse_number(input, i);
+  
+  if (!parse_pair(input, i)) {
+    skip_to_eol(input, i);
+    return 0;
+  }
+  
+  if (!parse_pair(input, i)) {
+    skip_to_eol(input, i);
+    return 0;
+  }
+
+  if (!parse_pair(input, i)) {
+    return 0;
+  }
+
+  return aunt;
+}
+
+static unsigned int parse(char *input) {
+  for (unsigned int i=0;; i++) {
+    if (input[i] == '\0') {
+      break;
+    }
+
+    unsigned int aunt = parse_line(input, &i);
+    if (aunt != 0) {
+      return aunt;
+    }
+    ASSERT(input[i] == '\n', "Did not parse full line");
+  }
+
+  FAIL("Did not find Aunt Sue");
+}
+
+static void solution1(char *input, char *output) {
+  akitas_ok = akitas_ok1;
+  cars_ok = cars_ok1;
+  cats_ok = cats_ok1;
+  children_ok = children_ok1;
+  goldfish_ok = goldfish_ok1;
+  perfumes_ok = perfumes_ok1;
+  pomeranians_ok = pomeranians_ok1;
+  samoyeds_ok = samoyeds_ok1;
+  trees_ok = trees_ok1;
+  vizslas_ok = vizslas_ok1;
+  
+  unsigned int aunt = parse(input);
+  snprintf(output, OUTPUT_BUFFER_SIZE, "%u", aunt);
+}
+
+static void solution2(char *input, char *output) {
+  akitas_ok = akitas_ok2;
+  cars_ok = cars_ok2;
+  cats_ok = cats_ok2;
+  children_ok = children_ok2;
+  goldfish_ok = goldfish_ok2;
+  perfumes_ok = perfumes_ok2;
+  pomeranians_ok = pomeranians_ok2;
+  samoyeds_ok = samoyeds_ok2;
+  trees_ok = trees_ok2;
+  vizslas_ok = vizslas_ok2;
+  
+  unsigned int aunt = parse(input);
+  snprintf(output, OUTPUT_BUFFER_SIZE, "%u", aunt);
+}
+
+int main(int argc, char *argv[]) {
+  return aoc_run(argc, argv, solution1, solution2);
+}
