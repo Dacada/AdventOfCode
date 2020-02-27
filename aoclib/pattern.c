2507d38aeea47bc54cfe6d753c30b6229f7b73d5
dacada <dacada@whatever>
2020-02-27 09:07:18 +0100

    Fixed pattern's indentation

--- aoclib/pattern.c
+++ aoclib/pattern.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 static void solution1(const char *const input, char *const output) {
-  (void)input;
-  snprintf(output, OUTPUT_BUFFER_SIZE, "NOT SOLVED");
+        (void)input;
+        snprintf(output, OUTPUT_BUFFER_SIZE, "NOT SOLVED");
 }
 
 static void solution2(const char *const input, char *const output) {
-  (void)input;
-  snprintf(output, OUTPUT_BUFFER_SIZE, "NOT SOLVED");
+        (void)input;
+        snprintf(output, OUTPUT_BUFFER_SIZE, "NOT SOLVED");
 }
 
 int main(int argc, char *argv[]) {
-  return aoc_run(argc, argv, solution1, solution2);
+        return aoc_run(argc, argv, solution1, solution2);
 }
