3e0d43caba24a078f6c17de7b2298efa2991d1cb
David Carrera <mail@davidcarreracasado.cat>
2024-12-15 16:05:19 +0100

    finally got around to solve day 13 p2 2024

--- 2024/13.c
+++ 2024/13.c
@@ -51,41 +51,41 @@ static struct machine *parse_input(const char *input, long *len) {
   return machines.data;
 }
 
-static void solution1(const char *input, char *const output) {
+static void solution(const char *input, char *const output, bool more) {
   long nmachines;
   struct machine *machines = parse_input(input, &nmachines);
 
   long total = 0;
   for (long i = 0; i < nmachines; i++) {
     struct machine machine = machines[i];
+    struct point v1 = machine.v1;
+    struct point v2 = machine.v2;
+    struct point v3 = machine.v3;
 
-    long a, b;
-    bool done = false;
-    for (a = 0; a < 100; a++) {
-      for (b = 0; b < 100; b++) {
-        if (machine.v1.x * a + machine.v2.x * b == machine.v3.x &&
-            machine.v1.y * a + machine.v2.y * b == machine.v3.y) {
-          done = true;
-          break;
-        }
-      }
-      if (done) {
-        break;
-      }
+    if (more) {
+      v3.x += 10000000000000L;
+      v3.y += 10000000000000L;
     }
 
-    if (done) {
-      total += 3 * a + b;
+    long n_v2 = (v1.x * v3.y - v1.y * v3.x) / (v2.y * v1.x - v2.x * v1.y);
+    long rem_v2 = (v1.x * v3.y - v1.y * v3.x) % (v2.y * v1.x - v2.x * v1.y);
+    long n_v1 = (v3.x - n_v2 * v2.x) / v1.x;
+    long rem_v1 = (v3.x - n_v2 * v2.x) % v1.x;
+    if (rem_v2 != 0 || rem_v1 != 0) {
+      continue;
     }
+    if (!more && (n_v1 > 100 || n_v2 > 100)) {
+      continue;
+    }
+    total += n_v1 * 3 + n_v2;
   }
 
   snprintf(output, OUTPUT_BUFFER_SIZE, "%ld", total);
   free(machines);
 }
 
-static void solution2(const char *input, char *const output) {
-  (void)input;
-  snprintf(output, OUTPUT_BUFFER_SIZE, "NOT SOLVED");
-}
+static void solution1(const char *input, char *const output) { solution(input, output, false); }
+
+static void solution2(const char *input, char *const output) { solution(input, output, true); }
 
 int main(int argc, char *argv[]) { return aoc_run(argc, argv, solution1, solution2); }
